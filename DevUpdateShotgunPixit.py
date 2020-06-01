74ae5d9cff5c61aaface04a62855dddb632d7938
sowrabh140892 <49732417+sowrabh140892@users.noreply.github.com>
2020-06-01 12:37:45 -0700

    Update DevUpdateShotgunPixit.py

--- DevUpdateShotgunPixit.py
+++ DevUpdateShotgunPixit.py
@@ -9,14 +9,16 @@ import sys
 import sentry_sdk
 import shotgun_api3
 from sentry_sdk.integrations.logging import ignore_logger
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
 sentry_sdk.init(dsn='https://2fee4ed938294813aeeb28f08e3614b8@sentry.io/1858927')
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-UAP_SNS_TOPIC="arn:aws:sns:ap-southeast-2:416748369696:UAPSNS"
+UAP_SNS_TOPIC="arn:aws:sns:us-east-1:398469702425:AASNS"
 
-BUCKET='aws-batch-parameter'
+BUCKET='aws-batch-parameters'
 KEY=str(sys.argv[1])
 session = boto3.Session()
 #print dir(session)
@@ -32,11 +34,8 @@ except Exception as e:
 finally:
     logger.info('Downloading Template Ended')
 
-
 file=open('/tmp/%s'%(KEY)).read().splitlines()
-
 print(file[count])
-
 line = file[count].split(' ')
 
 if len(line)==12:
@@ -60,6 +59,18 @@ elif len(line)==16:
 	PIXIT_MANIFEST_PATH=str(line[15])
 
 
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('CopyProcess-Log')
+
+response = table.query(KeyConditionExpression=Key('ProcessID').eq(SHOTGUN_ENTITY_ID))
+print("GetItem succeeded:")
+
+f = open('/tmp/'+SHOTGUN_ENTITY_ID+'.txt', 'w')
+
+for i in response['Items']:
+    f.write(str(i['ProcessNumber'])+"---"+i['Status']+"\n")
+
+f.close
 print(line)
 
 try:
@@ -77,13 +88,25 @@ def send_message_to_sns(topic_arn, message_body, message_attrs):
     :param message_attrs:
     :return:
     """
-    session = boto3.Session(region_name='ap-southeast-2')
+    session = boto3.Session(region_name='us-east-1')
     sns = session.client('sns')
     response = sns.publish(TopicArn=topic_arn,
                            Message=message_body,
                            MessageAttributes=message_attrs
                            )
     return response
+
+def update_shotgun(entity_id, entity_type, attr_name, attr_value):
+	sg = shotgun_api3.Shotgun(SHOTGUN_HOST_NAME, SHOTGUN_SCRIPT_NAME, SHOTGUN_SCRIPT_KEY)
+	attr_names = attr_name.split('|')
+	attr_values = attr_value.split('|')
+	logger.info(str(attr_names))
+	logger.info(str(attr_values))
+	if len(attr_names) == len(attr_values):
+		updates = dict(zip(attr_names, attr_values)) 
+		sg.update(entity_type, int(entity_id), updates)
+	else:
+		logger.error('parameters invalid')
 
 def main():
     """
@@ -93,23 +116,25 @@ def main():
     if len(line)==12:
 		msg_attr={}
 		msg_body = 'Changing Delivery Status to Delivered'
-		msg_attr['Type'] = {"DataType": "String", "StringValue": SHOTGUN_TYPE}
-		msg_attr['entityId'] = {"DataType": "Number", "StringValue": SHOTGUN_ENTITY_ID}
-		msg_attr['entityType'] = {"DataType": "String", "StringValue": SHOTGUN_ENTITY_TYPE}
-		msg_attr['attributeName'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_NAME}
-		msg_attr['attributeValue'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_VALUE}
-		response = send_message_to_sns(topic_arn=UAP_SNS_TOPIC, message_body=msg_body, message_attrs=msg_attr)
-		print(response)
+		update_shotgun(SHOTGUN_ENTITY_ID, SHOTGUN_ENTITY_TYPE, SHOTGUN_ATTRIBUTE_NAME, SHOTGUN_ATTRIBUTE_VALUE)
+		# msg_attr['Type'] = {"DataType": "String", "StringValue": SHOTGUN_TYPE}
+		# msg_attr['entityId'] = {"DataType": "Number", "StringValue": SHOTGUN_ENTITY_ID}
+		# msg_attr['entityType'] = {"DataType": "String", "StringValue": SHOTGUN_ENTITY_TYPE}
+		# msg_attr['attributeName'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_NAME}
+		# msg_attr['attributeValue'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_VALUE}
+		# response = send_message_to_sns(topic_arn=UAP_SNS_TOPIC, message_body=msg_body, message_attrs=msg_attr)
+		# print(response)
     elif len(line)==16:
 		msg_attr={}
 		msg_body = 'Changing Delivery Status to Delivered'
-		msg_attr['Type'] = {"DataType": "String", "StringValue": SHOTGUN_TYPE}
-		msg_attr['entityId'] = {"DataType": "Number", "StringValue": SHOTGUN_ENTITY_ID}
-		msg_attr['entityType'] = {"DataType": "String", "StringValue": SHOTGUN_ENTITY_TYPE}
-		msg_attr['attributeName'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_NAME}
-		msg_attr['attributeValue'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_VALUE}
-		response = send_message_to_sns(topic_arn=UAP_SNS_TOPIC, message_body=msg_body, message_attrs=msg_attr)
-		print(response)
+		update_shotgun(SHOTGUN_ENTITY_ID, SHOTGUN_ENTITY_TYPE, SHOTGUN_ATTRIBUTE_NAME, SHOTGUN_ATTRIBUTE_VALUE)
+		# msg_attr['Type'] = {"DataType": "String", "StringValue": SHOTGUN_TYPE}
+		# msg_attr['entityId'] = {"DataType": "Number", "StringValue": SHOTGUN_ENTITY_ID}
+		# msg_attr['entityType'] = {"DataType": "String", "StringValue": SHOTGUN_ENTITY_TYPE}
+		# msg_attr['attributeName'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_NAME}
+		# msg_attr['attributeValue'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_VALUE}
+		# response = send_message_to_sns(topic_arn=UAP_SNS_TOPIC, message_body=msg_body, message_attrs=msg_attr)
+		# print(response)
 		msg_attr={}
 		msg_body = 'Changing Delivery Status to Delivered'
 		msg_attr['ProjectType'] = {"DataType": "String", "StringValue": PIXIT_PROJECT_TYPE}
@@ -122,6 +147,8 @@ def main():
     sg = shotgun_api3.Shotgun(SHOTGUN_HOST_NAME, SHOTGUN_SCRIPT_NAME, SHOTGUN_SCRIPT_KEY)
     sg.create("Reply", {"entity": {"type": SHOTGUN_ENTITY_TYPE, "id": int(SHOTGUN_ENTITY_ID)},"content": "AWS Copy Completed..."})
     
+    sg = shotgun_api3.Shotgun(SHOTGUN_HOST_NAME, SHOTGUN_SCRIPT_NAME, SHOTGUN_SCRIPT_KEY)
+    sg.upload(SHOTGUN_ENTITY_TYPE, SHOTGUN_ENTITY_ID, '/tmp/'+SHOTGUN_ENTITY_ID+'.txt')
 	
 if __name__ == '__main__':
     main()
